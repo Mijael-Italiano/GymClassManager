22af13810e2575aa14fa6e349c4d14ccb6bb68ec
Mijael-Italiano <mijaelitaliano@gmail.com>
2026-01-17 20:27:31 -0300

    Initial commit - Gym class management system

---
+++ Entity/Clase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Entity
+{
+    public class Clase
+    {
+        public int Id_Clase { get; set; }
+        public Disciplina Disciplina { get; set; }
+        public int Cantidad_Inscriptos { get; set; }
+        public int CuotaMensual { get; set; }
+        public int Maximo_Alumnos { get; set; }
+    }
+}
---
+++ Entity/DetalleClase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class DetalleClase
+    {
+        public int Id_Detalle_Clases { get; set; }
+        public string Dia { get; set; }
+        public TimeSpan Horario_Inicio { get; set; }
+        public TimeSpan Horario_Fin { get; set; }
+
+        public Profesor? profesor { get; set; }
+        public Clase? clase { get; set; }
+
+    }
+}
